5fac48a02e9c0daa44391ae9910cc2e5b7948e0b
ianfreitas1 <ianfreitas_2005@hotmail.com>
2019-04-06 20:26:02 -0300

    Lista 1 completa
    Implementação de Cholesky e LU, com a solução de AX=B

--- exercicio1.py
+++ exercicio1.py
@@ -1,8 +1,8 @@
-import sys
+#import sys
 from math import sqrt
 
 
-A = [[16, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+A3 = [[16, 9, 8, 7, 6, 5, 4, 3, 2, 1],
      [9, 17, 9, 8, 7, 6, 5, 4, 3, 2],
      [8, 9, 18, 9, 8, 7, 6, 5, 4, 3],
      [7, 8, 9, 19, 9, 8, 7, 6, 5, 4],
@@ -11,15 +11,25 @@ A = [[16, 9, 8, 7, 6, 5, 4, 3, 2, 1],
      [4, 5, 6, 7, 8, 9, 16, 9, 8, 7],
      [3, 4, 5, 6, 7, 8, 9, 15, 9, 8],
      [2, 3, 4, 5, 6, 7, 8, 9, 14, 9],
-     [1, 2, 3, 4, 5, 6, 7, 8, 9, 13],]
-
-B = [[1,2,2],
+     [1, 2, 3, 4, 5, 6, 7, 8, 9, 13]]
+B3 = [4.0, 0.0, 8.0, 0.0, 12.0, 0.0, 8.0, 0.0, 4.0, 0.0]
+Alu = [[1,2,2],
      [4,4,2],
      [4,6,4]]
-C = [[1,0.2,0.4],
+Ua = [[1,2,2],
+     [0,-4,-6],
+     [0,0,-1]]
+Ya = [3,-6,1]
+La = [[1,0,0],
+      [4,1,0],
+      [4,0.5,1]]
+Blu = [3,6,10]
+Bch = [0.6,-0.3,-0.6]
+Ach = [[1,0.2,0.4],
      [0.2,1,0.5],
      [0.4,0.5,1]]
 
+# Funcao que retorna a propria matriz apos a decomposicao LU
 def decomposicaoLU(matriz):
     n = len(matriz)
     for k in range(n):
@@ -30,19 +40,62 @@ def decomposicaoLU(matriz):
                 matriz[i][j] = matriz[i][j]-matriz[i][k]*matriz[k][j]
     return matriz
 
-def decomposicaoCholesky(A):
+# Funcao que retorna a triangular inferior apos a decomposicao de Cholesky
+def Cholesky(A):
     n = len(A)
+    L = [[0.0] * n for i in range(n)]
     for i in range(n):
-        A[i][i] = sqrt(A[i][i])
-        for j in range(i+1, n):
-            A[j][i] = float(A[j][i]/A[i][i])
-        for k in range(i+1, n):
-            for j in range(k, n):
-                A[j][k] = A[j][k] - A[j][i]*A[k][i]
-    for a in range(n):
-        for b in range(n):
-            print A[a][b]
-    return A
+        for j in range(i+1):
+            s = sum(L[i][k]*L[j][k] for k in range(j))
+            L[i][j] = sqrt(A[i][i]-s) if (i==j) else (1.0/L[j][j]*(A[i][j]-s))
+    return L
 
-#decomposicaoLU(B)
-decomposicaoCholesky(C)
+# Funcao que realiza uma substituicao para frente para decomposicao LU
+def subFrente(L, B):
+    n = len(L)
+    Y = []
+    for i in range(n):
+        Y.append(0)
+    Y[0] = B[0]
+    for i in range(1,n):
+        s = 0
+        for j in range(i):
+            s = s + L[i][j]*Y[j]
+        Y[i] = B[i] - s
+    return Y
+
+# Funcao que realiza uma substituicao para frente para decomposicao de Cholesky
+def subFrente2(L,B):
+    n = len(L)
+    Y = []
+    for i in range(n):
+        Y.append(0)
+    for i in range(n):
+        a = B[i]
+        for j in range(i):
+            a = a - L[i][j]*Y[j]
+        Y[i] = float(a)/L[i][i]
+    return Y
+
+# Funcao que realiza uma retro-substituicao
+def subTras(U, Y):
+    n = len(U)
+    X = []
+    for i in range(n):
+        X.append(0)
+    X[n-1] = float(Y[n-1])/U[n-1][n-1]
+    for i in range(n-2, -1, -1):
+        s = Y[i]
+        for j in range(i+1, n):
+            s = s - U[i][j]*X[j]
+        X[i] = float(s)/U[i][i]
+    return X
+
+# Funcao que retorna a transposta da matriz passada como parametro
+def transposta(matriz):
+    n = len(matriz)
+    resp = [[0.0]*n for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            resp[j][i] = matriz[i][j]
+    return resp
---
+++ main.py
@@ -0,0 +1,89 @@
+from exercicio1 import *
+
+if __name__ == "__main__":
+    print "1: Decomposicao LU"
+    print "2: Decomposicao Cholesky"
+    print "3: Resolver AX = B utilizando LU"
+    print "4: Resolver AX = B utilizando Cholesky"
+    print "5: Resolver Exercicio 3 da lista 1"
+    resposta = int(input("Escolha um numero: "))
+    if resposta == 1:
+        tam = int(input("Escolha o tamanho da matriz quadrada: "))
+        matriz = [[0.0]*tam for i in range(tam)]
+        for i in range(tam):
+            for j in range(tam):
+                a = float(input("A" + str(i) + str(j) + ": "))
+                matriz[i][j] = a
+        print decomposicaoLU(matriz)
+    elif resposta == 2:
+        tam = int(input("Escolha o tamanho da matriz quadrada: "))
+        matriz = [[0.0]*tam for i in range(tam)]
+        for i in range(tam):
+            for j in range(tam):
+                a = float(input("A" + str(i) + str(j) + ": "))
+                matriz[i][j] = a
+        print "Triangular Inferior (L): " + str(Cholesky(matriz))
+        print "Triangular Superior (U): " + str(transposta(Cholesky(matriz)))
+    elif resposta == 3:
+        tam = int(input("Escolha o tamanho da matriz quadrada: "))
+        matriz = [[0.0]*tam for i in range(tam)]
+        for i in range(tam):
+            for j in range(tam):
+                a = float(input("A" + str(i) + str(j) + ": "))
+                matriz[i][j] = a
+        B = []
+        for i in range(tam):
+            b = float(input("B" + str(i) + ": "))
+            B.append(b)
+        A = decomposicaoLU(matriz)
+        print subTras(A, subFrente(A, B))
+    elif resposta == 4:
+        tam = int(input("Escolha o tamanho da matriz quadrada: "))
+        matriz = [[0.0]*tam for i in range(tam)]
+        for i in range(tam):
+            for j in range(tam):
+                a = float(input("A" + str(i) + str(j) + ": "))
+                matriz[i][j] = a
+        B = []
+        for i in range(tam):
+            b = float(input("B" + str(i) + ": "))
+            B.append(b)
+        L = Cholesky(matriz)
+        Y = subFrente2(L, B)
+        X = subTras(transposta(L), Y)
+        print "L = " + str(L)
+        print "Y = " + str(Y)
+        print "X = " + str(X)
+    elif resposta == 5:
+        A = [[16, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+             [9, 17, 9, 8, 7, 6, 5, 4, 3, 2],
+             [8, 9, 18, 9, 8, 7, 6, 5, 4, 3],
+             [7, 8, 9, 19, 9, 8, 7, 6, 5, 4],
+             [6, 7, 8, 9, 18, 9, 8, 7, 6, 5],
+             [5, 6, 7, 8, 9, 17, 9, 8, 7, 6],
+             [4, 5, 6, 7, 8, 9, 16, 9, 8, 7],
+             [3, 4, 5, 6, 7, 8, 9, 15, 9, 8],
+             [2, 3, 4, 5, 6, 7, 8, 9, 14, 9],
+             [1, 2, 3, 4, 5, 6, 7, 8, 9, 13]]
+        B = [4.0, 0.0, 8.0, 0.0, 12.0, 0.0, 8.0, 0.0, 4.0, 0.0]
+        print "Resolvendo AX=B utilizando decomposicao LU"
+        A2 = decomposicaoLU(A)
+        print subTras(A2, subFrente(A2, B))
+        print "----------------------------------------------"
+        print "Resolvendo AX=B utilizando decomposicao de Cholesky"
+        A = [[16, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+             [9, 17, 9, 8, 7, 6, 5, 4, 3, 2],
+             [8, 9, 18, 9, 8, 7, 6, 5, 4, 3],
+             [7, 8, 9, 19, 9, 8, 7, 6, 5, 4],
+             [6, 7, 8, 9, 18, 9, 8, 7, 6, 5],
+             [5, 6, 7, 8, 9, 17, 9, 8, 7, 6],
+             [4, 5, 6, 7, 8, 9, 16, 9, 8, 7],
+             [3, 4, 5, 6, 7, 8, 9, 15, 9, 8],
+             [2, 3, 4, 5, 6, 7, 8, 9, 14, 9],
+             [1, 2, 3, 4, 5, 6, 7, 8, 9, 13]]
+        B = [4.0, 0.0, 8.0, 0.0, 12.0, 0.0, 8.0, 0.0, 4.0, 0.0]
+        A3 = Cholesky(A)
+        Y = subFrente2(A3, B)
+        X = subTras(transposta(A3), Y)
+        print "Y = " + str(Y)
+        print "X = " + str(X)
