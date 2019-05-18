78b49cb611c14fdd73c3e640a591596ca3903469
ianfreitas1 <ianfreitas_2005@hotmail.com>
2019-05-17 21:55:40 -0300

    Richard extrapolation

--- Lista 7/lista7.py
+++ Lista 7/lista7.py
@@ -15,9 +15,33 @@ def derivadaCentral(funcao, x, deltaX):
     bottom = 2*deltaX
     return top/bottom
 
+def derivadaPassoFrenteRichard(funcao, x, deltaX, p):
+    d1 = derivadaPassoFrente(funcao, x, deltaX)
+    deltaX2 = deltaX/2
+    d2 = derivadaPassoFrente(funcao, x, deltaX2)
+    q = deltaX/deltaX2
+    resultado = d1 + (d1 - d2)/(q**(-p)-1)
+    return resultado
+
+def derivadaPassoTrasRichard(funcao, x, deltaX, p):
+    d1 = derivadaPassoTras(funcao, x, deltaX)
+    deltaX2 = deltaX/2
+    d2 = derivadaPassoTras(funcao, x, deltaX2)
+    q = deltaX/deltaX2
+    resultado = d1 + (d1 - d2)/(q**(-p)-1)
+    return resultado
+
+def derivadaCentralRichard(funcao, x, deltaX, p):
+    d1 = derivadaCentral(funcao, x, deltaX)
+    deltaX2 = deltaX/2
+    d2 = derivadaCentral(funcao, x, deltaX2)
+    q = deltaX/deltaX2
+    resultado = d1 + (d1 - d2)/(q**(-p)-1)
+    return resultado
+
 def funcao(x):
     return math.sin(x)
 
-print(derivadaPassoFrente(funcao, 1, 0.5))
-print(derivadaPassoTras(funcao, 1, 0.5))
-print(derivadaCentral(funcao, 1, 0.5))
+print(derivadaPassoFrenteRichard(funcao, 1, 0.5, 1))
+print(derivadaPassoTrasRichard(funcao, 1, 0.5, 1))
+print(derivadaCentralRichard(funcao, 1, 0.5, 1))
